9f12a6f8505074184a156209911825b006bc4776
edenbd1 <eden.baudin.invest@gmail.com>
2025-09-23 21:46:09 +0200

    Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaTwitter, FaGithub, FaDiscord } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 import '../styles/footer.css';
 import logoText from '../assets/images/logo_text.svg';
@@ -12,12 +13,12 @@ const Footer: React.FC = () => {
           <div className="footer-left">
             <img src={logoText} className="footer-logo" />
             <div className="footer-description">
-              <p>zlend is a decentralized peer-to-peer lending and borrowing protocol built on Aleo. Unlike traditional protocols, it leverages zero-knowledge technology to enable private transactions, with a liquidation mechanism based on collateral value. Users can lend and borrow ETH, USDC, and Aleo while preserving both privacy and security.</p>
+              <p>Making crypto tipping the default payment method for real-time creator monetization. Built on the XRP Ledger, it enables instant, low-cost, and secure payments directly on Twitch streams.</p>
             </div>
             <div className="footer-social">
-              <a href="https://discord.gg/VJkXvVKWfp" className="social-link" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
-              <a href="https://twitter.com/zLendfi" className="social-link" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-              <a href="https://github.com/zLend-fi" className="social-link" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+              <a href="https://www.linkedin.com/company/wavetip/" className="social-link" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+              <a href="https://x.com/wavetip_fi" className="social-link" target="_blank" rel="noopener noreferrer"><FaXTwitter /></a>
+              <a href="https://github.com/WaveTip" className="social-link" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
             </div>
           </div>
 
@@ -26,16 +27,16 @@ const Footer: React.FC = () => {
               <p>Ressources</p>
               <ul>
                 <li><a href="/#Home">Home</a></li>
-                <li><a href="/#Perks">Perks</a></li>
+                <li><a href="/#Demo">Demo</a></li>
                 <li><a href="/#FAQ">FAQ</a></li>
               </ul>
             </div>
             <div className="footer-right-section">
               <p>Ecosystem</p>
               <ul>
-                <li><a href="https://aleo.org/" target="_blank" rel="noopener noreferrer">Aleo</a></li>
-                <li><a href="https://provable.com/" target="_blank" rel="noopener noreferrer">Provable</a></li>
-                <li><a href="https://zsociety.io/" target="_blank" rel="noopener noreferrer">zSociety</a></li>  
+                <li><a href="https://ripple.com/solutions/stablecoin/?qgad=730464058942&qgterm=ripple%20payment%20network&utm_medium=ppc&utm_source=google&utm_term=ripple%20payment%20network&utm_campaign=Search+-+EU+-+Payments+-+Brand&hsa_acc=4920537092&hsa_cam=22162329059&hsa_grp=176996728707&hsa_ad=730464058942&hsa_src=g&hsa_tgt=kwd-968362428955&hsa_kw=ripple%20payment%20network&hsa_mt=p&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=22162329059&gbraid=0AAAAADRLBr-22gtzjYpuQWLy5dR4ctahx&gclid=CjwKCAjwisnGBhAXEiwA0zEOR02eSm05dvSguTGb9J6hXxND53y4INXH-EVFw1lRj6v59XnKUZw9JxoCxCMQAvD_BwE" target="_blank" rel="noopener noreferrer">RLUSD</a></li>
+                <li><a href="https://xrpl.org/" target="_blank" rel="noopener noreferrer">XRP Ledger</a></li>
+                <li><a href="https://web3auth.io/" target="_blank" rel="noopener noreferrer">Web3Auth</a></li>
               </ul>
             </div>
             <div className="footer-right-section">
@@ -43,7 +44,7 @@ const Footer: React.FC = () => {
               <ul>
                 <li><a href="/privacy">Privacy</a></li>
                 <li><a href="/terms">Terms</a></li>
-                <li><a href="mailto:contact@zlend.fi">Contact</a></li>
+                <li><a href="mailto:contact@wavetip.fi">Contact</a></li>
               </ul>
             </div>
           </div>
--- src/components/TableOfContents.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/tableOfContents.css';
-
-
-
-export default TableOfContents; 
